33e5104905063efc98e3fd6e015f21a85f63749a
Haren Lewis <harenlewis@gmail.com>
2024-03-01 22:29:04 -0500

    float truncation code

--- module_2/trunc.go
+++ module_2/trunc.go
@@ -1,10 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var value string
 	fmt.Println("Enter a floating point number: ")
 	fmt.Scan(&value)
-	fmt.Printf("Number is %s", value)
+	fmt.Println("Entered Number is", value)
+
+	float_value, err := strconv.ParseFloat(value, 64)
+	trunc_value := int(float_value)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Truncated Number is %s", strconv.Itoa(trunc_value))
+
 }
