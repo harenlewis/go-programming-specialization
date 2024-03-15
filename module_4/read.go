ef70555d7c350e063239afa80b8d0e804c7861b1
Haren Lewis <harenlewis@gmail.com>
2024-03-15 04:17:12 -0400

    file read - structs slice

--- module_4/read.go
+++ module_4/read.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Name struct {
+	FirstName string
+	LastName  string
+}
 
 func main() {
+	var filename string
+	name_slice := make([]Name, 0, 1)
+
 	fmt.Println("Enter file name:")
+
+	fmt.Scan(&filename)
+
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fullname := scanner.Text()
+		name_arr := strings.Split(fullname, " ")
+		name_slice = append(name_slice, Name{FirstName: name_arr[0], LastName: name_arr[1]})
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("The names are:")
+	for _, name := range name_slice {
+		fmt.Println(name.FirstName, name.LastName)
+	}
 }
